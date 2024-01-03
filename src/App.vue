f94ad0a926f979e8b6af80f13e9c6adea8d05105
William London <wl1959479486@icloud.com>
2024-01-03 20:20:38 +0800

    update nav bar and loading animate.

--- src/App.vue
+++ src/App.vue
@@ -254,7 +254,7 @@ function onClickBtn(btn_event) {
   content: '';
   height: 60px;
   position: absolute;
-  border-left: 12px solid orange;
+  border-left: 12px solid yellow;
   transform: rotate(-45deg);
 }
 
