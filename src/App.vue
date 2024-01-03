ed4c771bf0a058893a5462e7897e9037e3d8d4a0
William London <wl1959479486@icloud.com>
2024-01-03 20:19:42 +0800

    update nav bar and loading anamate.

--- src/App.vue
+++ src/App.vue
@@ -34,11 +34,50 @@ function onClickBtn(btn_event) {
 
 <template>
   <div>
-    <button id="btn_w" @click="onClickBtn">button to the William's page</button>
-    <button id="btn_j" @click="onClickBtn">button to the Jennifer's page</button>
-    <button id="btn_p" @click="onClickBtn">button to the PhotoWall</button>
-    <button id="btn_d" @click="onClickBtn">button to the DreamWall</button>
-    <button id="btn_r" @click="onClickBtn">return to StartPage</button>
+    <div class="header">
+      <button id="btn_r" class="btn" @click="onClickBtn">Home</button>
+      <button id="btn_w" class="btn" @click="onClickBtn">William's</button>
+      <button id="btn_j" class="btn" @click="onClickBtn">Jennifer's</button>
+      <button id="btn_p" class="btn" @click="onClickBtn">PhotoWall</button>
+      <button id="btn_d" class="btn" @click="onClickBtn">DreamWall</button>
+    </div>
+
+    <div class="loader">
+      <div class="container">
+        <div class="carousel">
+          <div class="love"></div>
+          <div class="love"></div>
+          <div class="love"></div>
+          <div class="love"></div>
+          <div class="love"></div>
+          <div class="love"></div>
+          <div class="love"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="carousel">
+          <div class="death"></div>
+          <div class="death"></div>
+          <div class="death"></div>
+          <div class="death"></div>
+          <div class="death"></div>
+          <div class="death"></div>
+          <div class="death"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="carousel">
+          <div class="robots"></div>
+          <div class="robots"></div>
+          <div class="robots"></div>
+          <div class="robots"></div>
+          <div class="robots"></div>
+          <div class="robots"></div>
+          <div class="robots"></div>
+        </div>
+      </div>
+    </div>
+
     <router-view v-slot="{ Component, route }">
       <transition name="fade" mode="out-in">
         <component v-bind:is="Component" />
@@ -48,18 +87,267 @@ function onClickBtn(btn_event) {
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+/*** header ***/
+.header {
+  position:fixed;
+  top: 0;
+  left: 0px;
+  width: 100%;
+  z-index: 999;
 }
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+/*** button ***/
+.btn {
+  font-size: 17px;
+  background: transparent;
+  border: none;
+  padding: 1em 1.5em;
+  color: #ffedd3;
+  text-transform: uppercase;
+  position: relative;
+  transition: 0.5s ease;
+  cursor: pointer;
 }
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.btn::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 2px;
+  width: 0;
+  background-color: #ffc506;
+  transition: 0.5s ease;
+}
+
+.btn:hover {
+  color: #1e1e2b;
+  transition-delay: 0.5s;
+}
+
+.btn:hover::before {
+  width: 100%;
+}
+
+.btn::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 0;
+  width: 100%;
+  background-color: #ffc506;
+  transition: 0.4s ease;
+  z-index: -1;
+}
+
+.btn:hover::after {
+  height: 100%;
+  transition-delay: 0.4s;
+  color: aliceblue;
+}
+
+.loader {
+  display: flex;
+  position: relative;
+  justify-items: center;
+  align-items: center;
+  gap: 1rem;
+  height: 55px;
+  width: 200px;
+  overflow: hidden;
+}
+
+.container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  height: 200px;
+  position: relative;
+  align-items: center;
+}
+
+.carousel {
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+  position: absolute;
+  width: 100%;
+  transform-origin: center;
+  animation-delay: 2s;
+}
+
+.loader .container:nth-child(3) {
+  justify-content: flex-start;
+  justify-items: flex-start;
+  animation: scroll-up 4s infinite ease-in-out;
+  animation-delay: 3s;
+}
+
+.loader .container:nth-child(2) {
+  justify-content: flex-end;
+  justify-items: flex-end;
+  animation: scroll-down 4s infinite ease-in-out;
+  animation-delay: 3s;
+}
+
+.loader .container:nth-child(1) {
+  justify-content: flex-end;
+  justify-items: flex-end;
+  animation: scroll-down 3s infinite ease-in-out;
+  animation-delay: 3s;
+}
+
+.love {
+  background: red;
+  display: flex;
+  width: 30px;
+  height: 30px;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  left: 8px;
+  margin: 0.8rem 4px;
+  transform: rotate(45deg);
+  animation-delay: 2s;
+}
+
+.love::before, .love::after {
+  content: '';
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background: red;
+}
+
+.love::before {
+  left: -16px;
+}
+
+.love::after {
+  top: -16px;
+}
+
+.death {
+  display: flex;
+  width: 100%;
+  height: 55px;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  animation: rotation 3s infinite ease-in-out;
+  animation-delay: 1s;
+}
+
+.death:after {
+  content: '';
+  height: 63px;
+  position: absolute;
+  border-left: 12px solid yellow;
+  transform: rotate(45deg);
+  border-radius: 8px;
+  top: -4px;
+}
+
+.death:before {
+  content: '';
+  height: 60px;
+  position: absolute;
+  border-left: 12px solid orange;
+  transform: rotate(-45deg);
+}
+
+.loader:hover {
+  animation: none;
+}
+
+.robots {
+  display: flex;
+  width: 40px;
+  height: 40px;
+  justify-content: space-between;
+  background-color: #0abab5;
+  border-radius: 0 8px 8px;
+  padding: 8px;
+  animation-delay: 5s;
+}
+
+.robots::after {
+  content: '';
+  width: 12px;
+  height: 12px;
+  top: 0;
+  left: 0;
+  background-color: #ffffff;
+  border-radius: 50%;
+  animation-delay: 2s;
+  animation: blink 0.5s 2 forwards;
+}
+
+.robots::before {
+  content: '';
+  width: 12px;
+  height: 12px;
+  top: 0;
+  left: 0;
+  background-color: #ffffff;
+  border-radius: 50%;
+  animation-delay: 2s;
+  animation: blink 0.5s 2 forwards;
+}
+
+@keyframes scroll-up {
+  0% {
+    transform: translateY(0);
+    filter: blur(0);
+  }
+
+  30% {
+    transform: translateY(-150%);
+    filter: blur(10px);
+  }
+
+  60% {
+    transform: translateY(0);
+    filter: blur(0px);
+  }
+}
+
+@keyframes scroll-down {
+  0% {
+    transform: translateY(0);
+    filter: blur(0);
+  }
+
+  30% {
+    transform: translateY(150%);
+    filter: blur(10px);
+  }
+
+  60% {
+    transform: translateY(0);
+    filter: blur(0px);
+  }
+}
+
+@keyframes rotation {
+  20%, 100% {
+    transform: rotate(180deg);
+  }
+}
+
+@keyframes blink {
+  0% {
+    height: 0;
+  }
+
+  20% {
+    height: 12px;
+  }
+
+  100% {
+    height: 12px;
+  }
 }
 </style>
