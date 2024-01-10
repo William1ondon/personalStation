e1bebe07d4bdfe1ff5cd4a5b8702011c9cbad0e8
William London <wl1959479486@icloud.com>
2024-01-10 16:41:13 +0800

    test video trans

--- src/oss.ts
+++ src/oss.ts
@@ -4,8 +4,8 @@ const client = new OSS({
     // yourregion填写Bucket所在地域。以华东1（杭州）为例，Region填写为oss-cn-hangzhou。
     region: 'oss-cn-guangzhou',
     // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-    accessKeyId: 'LTAI5tPJWm92JW8mZgd5cZyW',
-    accessKeySecret: 'fvT5tBKv46JC86zulnKPWZevWDwP7Q',
+    accessKeyId: 'LTAI5tLeYzYJHh4MMMzU2xTh',
+    accessKeySecret: 'bZyRc29P2rmb8MQxC751elnwrmiKwT',
     // yourbucketname填写存储空间名称。
     bucket: 'test-william-web'
 });
--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -2,21 +2,42 @@
 </script>
 
 <template>
-    <transition>
-        <div class="bg">
+    <!-- <transition> -->
+        <div class="video-box">
+            <video class="video-background" preload="auto" loop playsinline autoplay
+                src="https://test-william-web.oss-cn-guangzhou.aliyuncs.com/videos/web_bg/start_bg.mp4" tabindex="-1"
+                muted="muted"></video>
         </div>
-    </transition>
+    <!-- </transition> -->
 </template>
 
 <style scoped>
-.bg {
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.video-box {
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
     z-index: -1;
-    background: url("https://test-william-web.oss-cn-guangzhou.aliyuncs.com/img/web_bg/start_bg.jpg") no-repeat center center scroll;
-    background-size: cover;
+    /*进行视频裁剪*/
+    overflow: hidden;
+}
+
+.video-box .video-background {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    /*保证视频内容始终居中*/
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 100%;
+    /*保证视频充满屏幕*/
+    object-fit: cover;
+    min-height: 800px;
 }
 </style>
